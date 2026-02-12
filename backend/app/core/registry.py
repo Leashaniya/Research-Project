fbe83fb040f58f0541532f2fdd5f795b96c49f59
Leashaniya <it22891440@my.sliit.lk>
2026-02-13 00:59:15 +0530

    deleted unwanted files

--- backend/app/core/registry.py
+++
@@ -1,8 +0,0 @@
-from app.services import pastpaper_service, slides_service, structure_service, pipeline_service
-
-services = {
-    "pastpaper": pastpaper_service,
-    "slides": slides_service,
-    "structure": structure_service,
-    "pipeline": pipeline_service
-}
